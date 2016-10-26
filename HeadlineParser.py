ea61d3d9fd35917c5a14243effa6cf580e7fe27c
richardhensel <richardhensel20@gmail.com>
2016-10-26 22:21:13 +1000

    Work on named entity recognition

--- HeadlineParser.py
+++ HeadlineParser.py
@@ -3,8 +3,9 @@ import csv
 
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from nltk import tokenize
-from nltk import pos_tag, ne_chunk
+from nltk import pos_tag, ne_chunk, word_tokenize
 from nltk.tokenize import SpaceTokenizer
+from nltk.tree import Tree
 
 # A container class for storing company names information. 
 class CompanyNames():
@@ -13,84 +14,116 @@ class CompanyNames():
         self.asxCode = csvRow[1]
         self.gicsIndustryGroup = csvRow[2]
 
+
+# A container class for storing the output from the headline parser. 
+# Each company mentioned in a headline is assigned its own instance of this class. 
+class HeadlineResult():
+    def __init__(self, timestamp, companyCode, sentiment, keywordList):
+        self.timestamp = timestamp
+        self.companyCode = companyCode
+        self.sentiment = sentiment
+        self.keywordList = keywordList
+
+# Reads headlines, determines if company is mentioned and gauges sentiment. 
 class HeadlineParser():
 
     def __init__ (self, companyNamesPath):
-        self.companyNames = []
+        self.companyInfo = []
+        # Create the sentiment analyser. 
+        self.sentiment = SentimentIntensityAnalyzer()
+        self.tokenizer = SpaceTokenizer()
 
-        print('Importing company names data...')
+        #print('Importing company names data...')
         with open(os.path.join(companyNamesPath)) as localfile:
             reader = csv.reader(localfile,delimiter=',',quotechar='"')
             reader.next()
             for row in reader:
-                self.companyNames.append(CompanyNames(row))
+                self.companyInfo.append(CompanyNames(row))
  
 
     def parseHeadline(self, headline, dateString):
 
-        properNouns = self._getProperNouns(headlineString)
+        properNouns = self._getProperNouns(headline)
 
-        properNouns = self._getProperNouns(headlineString)
-        companyNames = self._getValidCompanyname(properNounList)
+        #companyNames = self._getValidCompanyName(headline)
+        #print companyNames
 
-        # Ignore headlines if more than one company is discussed. 
-        if len(companynames) >1:
-            quit()
-        else:
-            companyName = companyNames[0]
+        sentiment = self._getSentiment(headline)
+        resultList = []
 
-        sentiment = self._getSentiment(headlineString)
+        for name in properNouns:
+            resultList.append(HeadlineResult(dateString, name, sentiment, ''))
 
-        dateTime = dateString 
+        return resultList
 
+    
+    def _getValidCompanyName(self, headline):
+
+        wordList = self.tokenizer.tokenize(headline)
+
+        validNames = []        
+        print wordList
+        for word in wordList:
+            modifiedWord = word.lower().replace(' ', '')
+            print modifiedWord
+            for company in self.companyInfo:
+                modifiedName = company.companyName.lower().replace(' ', '')
+                if modifiedWord in modifiedName:
+                    validNames.append(company.asxCode)
+                    print modifiedName
+
+        
+        return validNames
+
+    ## Identify named entities within a string of text. 
+    # Return a list containing a string for each entity name. 
+    def _getProperNouns(self, text):
+        chunked = ne_chunk(pos_tag(word_tokenize(text)))
+        prev = None
+        continuous_chunk = []
+        current_chunk = []
+        for i in chunked:
+            if type(i) == Tree:
+                current_chunk.append(" ".join([token for token, pos in i.leaves()]))
+            elif current_chunk:
+                named_entity = " ".join(current_chunk)
+                if named_entity not in continuous_chunk:
+                    continuous_chunk.append(named_entity)
+                    current_chunk = []
+            else:
+                continue
+
+        return continuous_chunk
+
+    ## Identify the sentiment of a string of text. 
+    # Return a float between -1 and 1. 
     def _getSentiment(self, headlineString):
-        # Create the sentiment analyser. 
-        sid = SentimentIntensityAnalyzer()
 
         # Empty list to be filled with sentiment values. 
         sentimentList = []
         sentenceList = tokenize.sent_tokenize(headlineString)
 
         for sentence in sentenceList:
-            ss = sid.polarity_scores(sentence)
-            print ss['compound']
+            ss = self.sentiment.polarity_scores(sentence)
             sentimentList.append(ss['compound'])
 
         # Resulting sentiment is the average of each of the sentences in headline.
         return sum(sentimentList) / float(len(sentimentList)) 
 
-    def _getProperNouns(self, headlineString):
-
-        tokenizer = SpaceTokenizer()
-        toks = tokenizer.tokenize(headline)
-        print toks
-        pos = pos_tag(toks)
-        print pos
-        chunked_nes = ne_chunk(pos) 
-        print chunked_nes
-
-        nes = [' '.join(map(lambda x: x[0], ne.leaves())) for ne in chunked_nes if isinstance(ne, nltk.tree.Tree)]
-        print nes
-
-    def _getValidCompanyName(self, properNounsList):
-
-        return 1
+    def _getKeyWords(self, headlineString):
+        
 
 
 if __name__ == "__main__":
     
-    headline = "China weakens its stance on abortion. I feel good about it."
-    #headline = "I never said that it wasn't good. Yet, I could not want something more. Danger on the horizon. Explosion linked to operational defects. China weakens it's stance on abortion. US not willing to falter on climate change."
-
-    #headline = "Sabotage on SpaceX rocket explosion turns out to be possible"
-    #headline = "Altura mining stocks set to plummet"
-
-    #headline = "SpaceX's 'Raptor Rocket' to send humans to Mars"
-
     headlineparser = HeadlineParser('ASXListedCompanies.csv')
 
-    sentiment = headlineparser._getSentiment(headline)
-    print sentiment
+    dateString = '01_Jan_2016'
+
+    headlineList = ["China weakens its stance on abortion. I feel good about it.", "Explosion in Altura factory linked to operational defects.", "Sabotage on SpaceX rocket not yet ruled out.", "4 dead on Dreamworld ride.", "Bega's Barry Irvin can't find the magic formula to crack Chinese market", "Nescafe sued Moccona for $100m.", "Ardent CEO to address Dreamworld tragedy"]
+    for line in headlineList:
     
-
-
+        resultList = headlineparser.parseHeadline(line, dateString)
+        for result in resultList:
+            print result.companyCode
+            print result.sentiment
---
+++ NerTest.py
@@ -0,0 +1,32 @@
+
+from nltk import ne_chunk, pos_tag, word_tokenize
+from nltk.tree import Tree
+
+def get_continuous_chunks(text):
+    chunked = ne_chunk(pos_tag(word_tokenize(text)))
+    prev = None
+    continuous_chunk = []
+    current_chunk = []
+    for i in chunked:
+        print i
+        if type(i) == Tree:
+            print 'tree', i
+            current_chunk.append(" ".join([token for token, pos in i.leaves()]))
+        elif current_chunk:
+            named_entity = " ".join(current_chunk)
+            if named_entity not in continuous_chunk:
+                continuous_chunk.append(named_entity)
+                current_chunk = []
+        else:
+            continue
+
+    return continuous_chunk
+
+
+if __name__ == "__main__":
+
+#my_sent = "WASHINGTON -- In the wake of a string of abuses by New York police officers in the 1990s, Loretta E. Lynch, the top federal prosecutor in Brooklyn, spoke forcefully about the pain of a broken trust that African-Americans felt and said the responsibility for repairing generations of miscommunication and mistrust fell to law enforcement."
+    headlineString = "Boston Dynamics Finds Increasing Uptake In South Africa."
+    headlineString = "AT&T to buy Time Warner for $US85bn"
+    chunks = get_continuous_chunks(headlineString)
+    print chunks
